c776cbce62dd99ea520faeaa94a5ca1dd6ecb928
AndreaStradi <91668754+AndreaStradi@users.noreply.github.com>
2021-11-30 18:20:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+
+int funzione(int a){
+    int  divisori=1, divisori_interi=0;
+
+    while(divisori_interi<=1 && divisori<=a/2) {
+        if(a%divisori==0){
+            divisori_interi++;
+        }divisori++;}
+
+    if (divisori_interi==1)
+    {
+        return 1;
+    } else
+    {
+        return 0;
+    }}
 
